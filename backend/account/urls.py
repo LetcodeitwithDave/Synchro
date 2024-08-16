ab26926aeb02e5d12097beb3fd86cf80fcea4dd7
Dave <sololadavid4@gmail.com>
2024-08-16 12:29:51 +0100

    update

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from . import views
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+
+
 
 urlpatterns = [
+    path('signup/', views.Register, name='signup page'),
+    path('accountdetails/', views.Account, name='accountdetails'),
+    path('test/', views.test, name='test this thing')
         
 ]

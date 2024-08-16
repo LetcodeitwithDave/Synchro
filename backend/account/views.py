ded115e6bf3df0ee25cc7acc334a0bd95ebbc322
Dave <sololadavid4@gmail.com>
2024-08-16 12:28:37 +0100

    signup view

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from django.http import HttpResponse
+from .serializers import CustomUserSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
 
 # Create your views here.
 
+def test (request):
+    return HttpResponse('<h1> hello bro </h1>')
+
+@api_view(['POST'])
+def Register(request):
+    serializer = CustomUserSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({'success' :  'Account created successfully'}, status=status.HTTP_201_CREATED)
+    
+    return Response({'error' :  serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def Account(request):
+
+    context  = {
+        "username": request.user.username,
+        "email" : request.user.email,
+    }
+    return Response(context) 

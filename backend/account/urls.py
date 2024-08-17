15793a40ead4df3f664bb7131e3e7446211babd0
Dave <sololadavid4@gmail.com>
2024-08-17 16:54:31 +0100

    endpoint for token auth

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -5,6 +5,10 @@ from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 
 urlpatterns = [
+
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), #login
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), #refresh token
+
     path('signup/', views.Register, name='signup page'),
     path('accountdetails/', views.Account, name='accountdetails'),
     path('test/', views.test, name='test this thing')

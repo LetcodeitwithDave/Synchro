aa4d0072d665a2ce33e1d743a5ca4b554be77cc7
Dave <sololadavid4@gmail.com>
2024-08-17 23:10:24 +0100

    workspace and account route path

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('account.urls'))
+    path('api/', include('account.urls')),
+    path('api/', include('workspace.urls'))
 
 ]

61240019824c4dce92cc3c6701d909c33c7d0623
Dave <sololadavid4@gmail.com>
2024-08-18 21:41:27 +0100

    table for workspace model

--- backend/workspace/models.py
+++ backend/workspace/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from account.models import CustomUser
+
 
 # Create your models here.
+
+
+class Workspace(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)

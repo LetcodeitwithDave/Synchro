d5e210f191ebb096b7ed7e7bf55623fbba8d38b1
Dave <sololadavid4@gmail.com>
2024-08-18 21:40:34 +0100

    register workspace db

--- backend/workspace/admin.py
+++ backend/workspace/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+# from .models import Workspace
+from .models import Workspace
 
 # Register your models here.
+
+# admin.site.register(Workspace)
+admin.site.register(Workspace)

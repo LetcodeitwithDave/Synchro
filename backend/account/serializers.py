d769691500fd0183fcebce558ae0b2b3b747cf20
Dave <sololadavid4@gmail.com>
2024-08-19 14:25:49 +0100

    profile serilizer for user profile

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -7,7 +7,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model =  CustomUser
-        fields  = ['email', 'username', 'password', 'password2', 'fullname']
+        fields  = ['id', 'email', 'username', 'password', 'password2', 'fullname']
 
     def validate(self, data):
         if data['password'] != data['password2']:
@@ -20,3 +20,22 @@ class CustomUserSerializer(serializers.ModelSerializer):
         user  =  CustomUser.objects.create_user(**validated_data)
         return user
 
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'username', 'fullname', 'profile_picture', 'bio',]
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.fullname = validated_data.get('fullname', instance.fullname)
+        instance.bio = validated_data.get('bio', instance.bio)
+
+
+
+        if 'profile_picture' in validated_data:
+            instance.profile_picture =  validated_data['profile_picture']
+
+        instance.save()
+
+        return instance
+        return super().update(instance, validated_data)

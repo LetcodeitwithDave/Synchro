d71492bf2b6e883e36bd9090eb300fc73bf30817
Dave <sololadavid4@gmail.com>
2024-08-20 23:47:41 +0100

    AuthProvider wrapper

--- frontend/Synchro/src/App.jsx
+++ frontend/Synchro/src/App.jsx
@@ -1,5 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
+import { AuthProvider } from "./utils/auth";
+import { Signin, Signup } from "./pages";
+
 function App() {
-  return <>hello</>;
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route element={<Signup />} path="/register" />
+          <Route element={<Signin />} path="/login" />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
 }
 
 export default App;

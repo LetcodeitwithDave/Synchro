869ec5e2fb59567aa3ba0ebe31d5a2061b2978eb
Dave <sololadavid4@gmail.com>
2024-08-20 23:46:30 +0100

    tailwind config

--- frontend/Synchro/tailwind.config.js
+++ frontend/Synchro/tailwind.config.js
@@ -5,7 +5,49 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      robotoRegular: ['Roboto Regular', 'sans-serif'],
+      robotoMedium: ['Roboto Medium', 'sans-serif'],
+      robotoBold: ['Roboto Bold', 'sans-serif'],
+      montserratRegular: ['Montserrat Regular', 'sans-serif'],
+      montserratBold: ['Montserrat Bold', 'sans-serif'],
+      montserratSemibold: ['Montserrat SemiBold', 'sans-serif'],
+
+    },
+    extend: {
+
+      keyframes: {
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+
+      animation: {
+        spin: 'spin 1s linear infinite',
+      },
+
+      borderColor: {
+        customColor1: '#007BFF',  // Custom color (lightblue)
+      },
+      colors: {
+        test :  '#FAFAFB',
+        pageBackground : '#F8F9FA',
+        heroBackground: '#F2F2F2',
+        textColor: '#333333',
+        subheading: '#666666',
+        buttonBackground: '#007BFF',
+        OfferSectionBackground: '#EFEFEF',
+      },
+
+      textDecorationColor: {
+        decorationColor: '#007BFF',
+      },
+
+      backgroundImage: {
+        'header': "url('assets/images/pxfuel.jpg')",
+      },
+    },
   },
   plugins: [],
 }

924a0d15d2eca538d65cf4ef466f0c549bf2113d
Dave <sololadavid4@gmail.com>
2024-08-28 23:09:13 +0100

    auth context

--- frontend/Synchro/src/utils/authcontext.jsx
+++ frontend/Synchro/src/utils/authcontext.jsx
@@ -1,6 +1,14 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 const AuthContext = createContext();
 const useAuth = () => useContext(AuthContext);
 
-const AuthProvider = ({ children }) => {};
+const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState();
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};

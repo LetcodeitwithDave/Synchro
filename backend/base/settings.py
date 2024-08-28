c2066474a0c5b377e5bf969746f5c3ce71b58cf7
Dave <sololadavid4@gmail.com>
2024-08-28 21:59:42 +0100

    update app

--- backend/base/settings.py
+++ backend/base/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
 
     #Apps
     'account',
-    'workspace',
+    # 'workspace',
 
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist', #blacklist after refresh

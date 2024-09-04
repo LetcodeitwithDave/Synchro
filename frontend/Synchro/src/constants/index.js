00acfb807bf9b8c1eda69e42394638ae6c28d445
Dave <sololadavid4@gmail.com>
2024-09-04 21:41:38 +0100

    cons for sidenav

---
+++ frontend/Synchro/src/constants/index.js
@@ -0,0 +1,7 @@
+export const Navlinks =[
+    {'label': 'Dashboard', 'href': '/home'},
+    {'label': 'My Documents', 'href': '/documents'},
+    {'label': 'Upload', 'href': '/upload'},
+    {'label': 'Tags & Categories', 'href': '/tags'},
+    {'label': 'Recent Activity', 'href': '/activity'}
+]

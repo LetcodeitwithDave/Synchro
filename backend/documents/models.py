dfe2639246e7cd5ecbfa7be35c94ff190ba4f7a3
Dave <sololadavid4@gmail.com>
2024-09-07 00:25:30 +0100

    change model name

--- backend/documents/models.py
+++ backend/documents/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Document(models.Model):
+class FileUpload(models.Model):
     title = models.CharField(max_length=100)
     descrition = models.CharField(max_length=500)
     tags = models.CharField(max_length=100)

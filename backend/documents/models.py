611bfc5db85ce82fcb577d40d6bddbf32106382d
Dave <sololadavid4@gmail.com>
2024-09-07 00:12:27 +0100

    file upload model

--- backend/documents/models.py
+++ backend/documents/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Document(models.Model):
+    title = models.CharField(max_length=100)
+    descrition = models.CharField(max_length=500)
+    tags = models.CharField(max_length=100)
+    file_upload =  models.FileField(upload_to='media')

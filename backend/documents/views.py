3558c450f83538bddb6f5c91b2e7fd246c475006
Dave <sololadavid4@gmail.com>
2024-09-07 14:50:11 +0100

    list of document

--- backend/documents/views.py
+++ backend/documents/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .serializers import FileUploadSerializer
+from .models import FileUpload
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
 
 # Create your views here.
+
+@api_view(['GET'])
+def mydocuments(request):
+    query = FileUpload.objects.all()
+    if request.method == 'GET':
+        
+        serializer = FileUploadSerializer(query, many= True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        

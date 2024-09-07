8899ce5abd8ee2ff9d59fcd5d66bf92d88ae5dc2
Dave <sololadavid4@gmail.com>
2024-09-07 02:00:26 +0100

    update model fields

--- backend/documents/models.py
+++ backend/documents/models.py
@@ -1,9 +1,27 @@
 from django.db import models
-
+from account.models import CustomUser
 # Create your models here.
 
 class FileUpload(models.Model):
     title = models.CharField(max_length=100)
-    descrition = models.CharField(max_length=500)
-    tags = models.CharField(max_length=100)
-    file_upload =  models.FileField(upload_to='media')
+    description = models.TextField(max_length=500)
+    tags = models.ManyToManyField('Tag', related_name='tags')
+    file =  models.FileField(upload_to='documents/')
+    uploader = models.ForeignKey(CustomUser, null=True , on_delete=models.CASCADE, related_name='documents')
+    last_updated = models.DateTimeField( null=True , auto_now_add=True)
+    upload_date = models.DateTimeField( null=True, auto_now_add=True)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self) -> str:
+        return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)

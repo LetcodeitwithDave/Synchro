9dc33c8f5d5bda55558f958aaa8a9e20299af865
Dave <sololadavid4@gmail.com>
2024-09-08 15:32:49 +0100

    register tag, category model

--- backend/documents/admin.py
+++ backend/documents/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import FileUpload
+from .models import Document, Tag, Category
 # Register your models here.
 
 
-admin.site.register(FileUpload)
+admin.site.register(Document)
+admin.site.register(Tag)
+admin.site.register(Category)

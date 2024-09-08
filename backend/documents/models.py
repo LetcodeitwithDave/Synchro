87b68d288de9a700f28dbec612600c485ec5f254
Dave <sololadavid4@gmail.com>
2024-09-08 15:37:41 +0100

    Tag model

--- backend/documents/models.py
+++ backend/documents/models.py
@@ -2,10 +2,17 @@ from django.db import models
 from account.models import CustomUser
 # Create your models here.
 
-class FileUpload(models.Model):
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Document(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
-    tags = models.ManyToManyField('Tag', related_name='tags')
+    tags = models.ManyToManyField(Tag, related_name='tags')
     file =  models.FileField(upload_to='documents/')
     uploader = models.ForeignKey(CustomUser, null=True , on_delete=models.CASCADE, related_name='documents')
     last_updated = models.DateTimeField( null=True , auto_now_add=True)
@@ -16,12 +23,11 @@ class FileUpload(models.Model):
         return self.title
 
 
-class Tag(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self) -> str:
-        return self.name
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+    

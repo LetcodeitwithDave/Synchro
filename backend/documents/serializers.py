ba257da6ab4b3e0f3b5136269a1d582f78965fb9
Dave <sololadavid4@gmail.com>
2024-09-09 00:51:06 +0100

    update DocumentSerializer to get complete file url

--- backend/documents/serializers.py
+++ backend/documents/serializers.py
@@ -1,9 +1,33 @@
-from  .models import Document
+from  .models import Document, Tag, Category
 from rest_framework import serializers
 
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =  Tag
+        fields =  ['name']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model =  Category
+        fields =  ['name']
 
 class DocumentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Document
         fields = '__all__'
+
+
+class DocumentSummarySerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True)
+    category = CategorySerializer(many=True)
+    file = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Document
+        fields = ['title', 'description', 'tags','category' ,'file']
+
+    
+    def get_file(self, obj):
+        return self.context['request'].build_absolute_uri(obj.get_absolute_url())

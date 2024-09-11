116c195d7e9d2127cd872d6a03d2494524c4997a
Dave <sololadavid4@gmail.com>
2024-09-11 21:38:50 +0100

    fetch latest files

--- frontend/Synchro/src/pages/Dashboard.jsx
+++ frontend/Synchro/src/pages/Dashboard.jsx
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react";
 import Sidebar from "../components/Sidebar";
-
+import { UploadButton } from "../components/uploadButton";
 export default function Dashboard() {
+  const [files, setFiles] = useState([]);
+
+  const FetchFiles = async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:8000/api/dashboard/document_summary",
+        { method: "GET" }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("response data ", data);
+      }
+    } catch (error) {
+      console.log("something went wrong");
+    }
+  };
+
+  useEffect(() => {
+    FetchFiles();
+  }, []);
+
   return (
-    <div>
-      <Sidebar />;
+    <div className=" flex">
+      <div>
+        <Sidebar />;
+      </div>
+
+      <div className="   flex-row mt-12  justify-between flex-1 flex mx-10">
+        <div className=" font-rubikRegalar  text-2xl">YOUR RECENT FILES</div>
+
+        <UploadButton />
+      </div>
     </div>
   );
 }

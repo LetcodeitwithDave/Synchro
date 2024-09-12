ae03d46b7eee0e9962f4b19814cfc99362649e74
Dave <sololadavid4@gmail.com>
2024-09-12 13:41:13 +0100

    move upload logic to seperate file for simplicity

---
+++ frontend/Synchro/src/utils/UploadLogic.jsx
@@ -0,0 +1,40 @@
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const UploadLogic = async (uploadDetails, selectedFile) => {
+  // Ensure all state is up-to-date before uploading
+  console.log("Upload details: ", uploadDetails);
+  console.log("Selected file: ", selectedFile);
+
+  const uploadData = new FormData(); // Create a new FormData object
+
+  // Append text and file data to the FormData object
+  uploadData.append("title", uploadDetails.title);
+  uploadData.append("description", uploadDetails.description);
+  uploadData.append("tags", uploadDetails.tags);
+  uploadData.append("category", uploadDetails.category);
+  uploadData.append("file", selectedFile);
+  console.log("uploadded data ", uploadData);
+
+  try {
+    const response = await fetch(
+      "http://localhost:8000/api/dashboard/document_summary",
+      {
+        method: "POST",
+        body: uploadData, // Send FormData directly
+        // Content-Type header should not be manually set as the browser will handle it
+      }
+    );
+
+    if (response.ok) {
+      toast.success("File Upload Successfully");
+    } else {
+      const errorData = await response.json();
+      console.error("Upload failed: ", errorData);
+      toast.error("File upload failed");
+    }
+  } catch (error) {
+    console.error("Error uploading file: ", error);
+    toast.error("Error uploading file");
+  }
+};

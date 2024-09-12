5c6a4a03a6015bde8a7f4aecb58d32cc752a2a00
Dave <sololadavid4@gmail.com>
2024-09-12 12:04:14 +0100

    fix issue with sending data

--- backend/documents/serializers.py
+++ backend/documents/serializers.py
@@ -22,30 +22,29 @@ class DocumentSerializer(serializers.ModelSerializer):
 
 
 class DocumentSummarySerializer(serializers.ModelSerializer):
-    tags = TagSerializer(many=True)
-    category = CategorySerializer()
+    
 
     class Meta:
         model = Document
-        fields = ['title', 'description', 'tags', 'category', 'file']
+        fields = ['title', 'description', 'file']
 
-    def create(self, validated_data):
-        # Extract category and tags from validated_data
-        category_data = validated_data.pop('category')
-        tag_data = validated_data.pop('tags', [])
+    # def create(self, validated_data):
+    #     # Extract category and tags from validated_data
+    #     category_data = validated_data.pop('category')
+    #     tag_data = validated_data.pop('tags', [])
 
-        # Handle category creation/retrieval
-        category, _ = Category.objects.get_or_create(**category_data)
+    #     # Handle category creation/retrieval
+    #     category, _ = Category.objects.get_or_create(**category_data)
 
-        # Create the document object
-        document = Document.objects.create(category=category, **validated_data)
+    #     # Create the document object
+    #     document = Document.objects.create(category=category, **validated_data)
 
-        # Handle tags: get or create each tag and associate it with the document
-        for tag in tag_data:
-            tag_instance, _ = Tag.objects.get_or_create(**tag)
-            document.tags.add(tag_instance)
+    #     # Handle tags: get or create each tag and associate it with the document
+    #     for tag in tag_data:
+    #         tag_instance, _ = Tag.objects.get_or_create(**tag)
+    #         document.tags.add(tag_instance)
 
-        return document
+    #     return document
 
         
     

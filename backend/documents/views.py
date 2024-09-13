3ab025cda9b5ea9a199e424f4ab17d15251f81e2
Dave <sololadavid4@gmail.com>
2024-09-13 17:51:29 +0100

    logic for request data creationg

--- backend/documents/views.py
+++ backend/documents/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .serializers import DocumentSerializer , DocumentSummarySerializer
-from .models import Document
+from .models import Document, Category, Tag
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.decorators import api_view, permission_classes, parser_classes
 from rest_framework.permissions import IsAuthenticated
@@ -42,12 +42,32 @@ def document_summary (request):
 
 
     if request.method ==  'POST':
+        data  = request.data
+        print('Received data ->', data)
+        print('Received data ->', data.get('tags'))
 
-        print('Received data ->', request.data)
-        serializer = DocumentSummarySerializer(data=request.data)
 
-        if serializer.is_valid():
-            serializer.save()
+        cateogry, created = Category.objects.get_or_create(name =  data['category'])
+        
+        tag, created = Tag.objects.get_or_create(name =  data['tags'])
+            
+
+        print(' this is the tags --', tag)
+            
+
+        if cateogry and tag :
+            print('categeory in from view request ===> ', cateogry)
+
+            document =  Document.objects.create(
+                title= data['title'], 
+                description =  data['description'],
+                category = cateogry,
+                tags =  tag
+            )
+
+            serializer = DocumentSummarySerializer(document)
+
+            
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        print(' serializer error -> ', serializer.errors)
+            print(' serializer error -> ', serializer.errors)
         return Response( serializer.errors ,status=status.HTTP_400_BAD_REQUEST)

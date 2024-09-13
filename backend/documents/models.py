20fd71a6c7c9c0da501e44871084250dece9054b
Dave <sololadavid4@gmail.com>
2024-09-13 17:51:47 +0100

    update

--- backend/documents/models.py
+++ backend/documents/models.py
@@ -5,23 +5,23 @@ from account.models import CustomUser
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, null=True, blank=True)
+    name = models.CharField(max_length=100)
 
-    def __str__(self) -> str:
-        return self.name
+    def __str__(self) :
+        return self.name 
     
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=100, null=True , blank=True)
+    name = models.CharField(max_length=100, null=True,  blank=True)
 
-    def __str__(self) -> str:
+    def __str__(self) :
         return self.name
 
 class Document(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
-    tags = models.ManyToManyField(Tag, related_name='tags', blank=True)
+    tags = models.ForeignKey(Tag, related_name='tags', null=True , on_delete=models.SET_NULL)
     file =  models.FileField(upload_to='documents/', null=True, blank=True)
     uploader = models.ForeignKey(CustomUser, null=True , on_delete=models.CASCADE, related_name='documents')
     last_updated = models.DateTimeField( null=True , auto_now_add=True)
@@ -34,7 +34,7 @@ class Document(models.Model):
     
 
 
-    def __str__(self) -> str:
+    def __str__(self) :
         return self.title
 
 

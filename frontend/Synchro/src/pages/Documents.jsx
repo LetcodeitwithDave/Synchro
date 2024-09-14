47d81511ce177caa3679950b769b2c460e018761
Dave <sololadavid4@gmail.com>
2024-09-14 17:43:25 +0100

    Document page

---
+++ frontend/Synchro/src/pages/Documents.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+
+function Documents() {
+  return (
+    <div className=" flex">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default Documents;

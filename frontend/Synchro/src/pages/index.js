06c201cc75ac2ca8ba8f252feb1e1e4e94dc2766
Dave <sololadavid4@gmail.com>
2024-09-14 17:48:27 +0100

    import Documents pages

--- frontend/Synchro/src/pages/index.js
+++ frontend/Synchro/src/pages/index.js
@@ -3,11 +3,13 @@ import Signup from "./Signup";
 import Dashboard from "./Dashboard";
 import Upload from "./Upload";
 import FileUploadAndList from "./Dummy";
+import Documents from "./Documents";
 
 export {
     Signin,
     Signup,
     Dashboard,
     Upload,
-    FileUploadAndList
+    FileUploadAndList,
+    Documents
 }

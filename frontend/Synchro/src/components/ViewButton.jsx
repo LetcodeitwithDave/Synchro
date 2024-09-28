90bb743c27c793dc0fa6848038bfa2004bae9476
Dave <sololadavid4@gmail.com>
2024-09-28 09:42:45 +0100

    inital commit

---
+++ frontend/Synchro/src/components/ViewButton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ViewButton() {
+  return (
+    <div>
+      <button className=" rounded-full bg-secondaryColor font-rubikRegalar text-sm px-4 py-2 text-white">
+        View
+      </button>
+    </div>
+  );
+}
+
+export default ViewButton;

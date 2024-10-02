f34e7682a4fe39d9e699f5881bf7c3fe81d420c3
Dave <sololadavid4@gmail.com>
2024-10-02 21:25:16 +0100

    download logic for document

--- frontend/Synchro/src/components/DownloadButton.jsx
+++ frontend/Synchro/src/components/DownloadButton.jsx
@@ -1,9 +1,50 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function DownloadButton() {
+function DownloadButton({ items, fileId, fileName }) {
+  const downloadDocument = async () => {
+    // const API_URL =
+
+    try {
+      const response = await fetch(
+        `http://localhost:8000/api/download_document/${fileId}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        console.log("failed to download file");
+      }
+
+      const data = await response.blob(); //convert response to blob
+
+      const downloadUrl = window.URL.createObjectURL(data); //DOM url
+      const link = document.createElement("a");
+      link.href = downloadUrl;
+      link.download = fileName;
+
+      link.click();
+      link.remove();
+      //make DOM url invalid
+      window.URL.revokeObjectURL(downloadUrl);
+
+      console.log(link);
+      console.log(" blob document (converted response to blob) -> ", data);
+      console.log("download url -> ", downloadUrl);
+    } catch (error) {
+      console.error("Download failed ", error.message);
+    }
+  };
   return (
     <div>
-      <button className=" flex  gap-1 items-center rounded-full bg-red-600 font-rubikRegalar text-sm px-4 py-1 text-white">
+      <button
+        onClick={downloadDocument}
+        className=" flex  gap-1 items-center rounded-full bg-red-600 font-rubikRegalar text-sm px-4 py-1 text-white"
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"

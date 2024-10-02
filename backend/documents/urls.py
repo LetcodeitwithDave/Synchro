e41d5856df90549897576aeb03b00b1071bffcff
Dave <sololadavid4@gmail.com>
2024-10-02 05:27:51 +0100

    document download route

--- backend/documents/urls.py
+++ backend/documents/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("documents", views.mydocuments , name="documents list"),
+    path("download_document/<int:file_id>", views.download_document, name="download file")
 ]

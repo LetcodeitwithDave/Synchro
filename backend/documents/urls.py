bf7ac73d8d05845ecdaa33d1cf2eacf6d74d4730
Dave <sololadavid4@gmail.com>
2024-10-02 04:33:15 +0100

    .

--- backend/documents/urls.py
+++ backend/documents/urls.py
@@ -2,5 +2,4 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("documents", views.mydocuments , name="documents list"),
-    path("dashboard/document_summary", views.document_summary , name="documents summary"),
 ]

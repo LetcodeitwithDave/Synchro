d90681b6ad6df32b74cc88164d403cf0673c4c30
Dave <sololadavid4@gmail.com>
2024-10-04 17:05:48 +0100

    .

--- backend/documents/urls.py
+++ backend/documents/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("documents", views.mydocuments , name="documents list"),
-    path("download_document/<int:file_id>", views.download_document, name="download file")
+    path("download_document/<int:file_id>", views.download_document, name="download file"),
+    path("document/search", views.search_document, name='search document' )
 ]

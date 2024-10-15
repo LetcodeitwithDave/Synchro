9b5976136afe99359d6e7eba8761df289cb30455
Dave <sololadavid4@gmail.com>
2024-10-15 21:27:42 +0100

    updated

--- frontend/Synchro/src/pages/Documents.jsx
+++ frontend/Synchro/src/pages/Documents.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
-
+import { UploadButton } from "../components/uploadButton";
+import NavContent from "../components/NavContent";
 function Documents() {
   return (
     <div className=" flex">
       <Sidebar />
+
+      <div className=" flex-col mt-12   flex-1 flex mx-10">
+        <div className=" flex flex-row justify-between">
+          <div className=" font-rubikRegalar text-2xl">ALL FILES</div>
+
+          <UploadButton />
+        </div>
+        <div className=" mt-8 flex">
+          <NavContent />
+        </div>
+      </div>
     </div>
   );
 }

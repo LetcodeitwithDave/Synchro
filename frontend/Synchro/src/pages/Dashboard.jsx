5593a9670eed333a3d0994758fbaa3a32381d70f
Dave <sololadavid4@gmail.com>
2024-10-15 21:44:37 +0100

    update codebase

--- frontend/Synchro/src/pages/Dashboard.jsx
+++ frontend/Synchro/src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Search } from "../components";
 import DownloadButton from "../components/DownloadButton";
 import ViewButton from "../components/ViewButton";
 import Sidebar from "../components/Sidebar";
@@ -11,15 +12,16 @@ export default function Dashboard() {
   const FetchFiles = async () => {
     try {
       const response = await fetch(
-        "http://localhost:8000/api/dashboard/document_summary",
+        "http://localhost:8000/api/dashboard/dashboard_summary/",
         { method: "GET" }
       );
 
       if (response.ok) {
         const data = await response.json();
         setFiles(data);
-        console.log("files -> ", files);
-        console.log("response data ", data);
+
+        // console.log("files -> ", files);
+        // console.log("response data ", data);
       }
     } catch (error) {
       console.log("something went wrong");
@@ -37,6 +39,9 @@ export default function Dashboard() {
           <Sidebar />;
         </div>
         <div className="   flex-col mt-12   flex-1 flex mx-10">
+          <div className=" mb-10">
+            <Search />
+          </div>
           <div className=" flex flex-row  justify-between">
             <div className=" font-rubikRegalar text-gray-900  text-2xl">
               YOUR RECENT FILES
@@ -88,10 +93,10 @@ export default function Dashboard() {
                           viewBox="0 0 24 24"
                           fill="none"
                           stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          class="lucide lucide-ellipsis-vertical"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          className="lucide lucide-ellipsis-vertical"
                         >
                           <circle cx="12" cy="12" r="1" />
                           <circle cx="12" cy="5" r="1" />
@@ -111,7 +116,7 @@ export default function Dashboard() {
 
                   <div className="  flex gap-2">
                     <ViewButton items={items.file} />
-                    <DownloadButton />
+                    <DownloadButton fileId={items.id} fileName={items.title} />
                   </div>
                 </div>
               </div>

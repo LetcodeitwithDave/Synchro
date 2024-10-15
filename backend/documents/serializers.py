2075175203a98c62b4d9300b12cd75feb73343a7
Dave <sololadavid4@gmail.com>
2024-10-15 21:57:11 +0100

    .

--- backend/documents/serializers.py
+++ backend/documents/serializers.py
@@ -34,24 +34,7 @@ class DocumentSummarySerializer(serializers.ModelSerializer):
         model = Document
         fields = ['title', 'description', 'tags', 'file' , 'category']
     
-    def create(self, validated_data):
-
-        
-        tags_data = validated_data.pop('tags')
-        category_data = validated_data.pop('category')
-
-        
-        
-        tag_instance = Tag.objects.create(name = tags_data)
-        category_instance = Category.objects.create(name = category_data)
-
-        document =  Document.objects.create(
-            category= category_instance, 
-            tags= tag_instance , 
-            **validated_data
-            )
-        
-        return document
+    
     
 
 class SearchSerializer(serializers.ModelSerializer):

da488fa8126f67a0628dac7650420169e3a6ae0a
Dave <sololadavid4@gmail.com>
2024-11-15 23:32:20 +0100

    add icon to array

--- frontend/Synchro/src/constants/index.js
+++ frontend/Synchro/src/constants/index.js
@@ -1,7 +1,10 @@
+import { LayoutDashboard, FolderOpen, Video, Images, Goal } from "lucide-react"
 export const Navlinks =[
-    {'label': 'Dashboard', 'href': '/home'},
-    {'label': 'My Documents', 'href': '/documents'},
-    {'label': 'Upload', 'href': '/upload'},
-    {'label': 'Tags & Categories', 'href': '/tags'},
-    {'label': 'Recent Activity', 'href': '/activity'}
+    {'label': 'Dashboard', 'href': '/home', 'icon' : LayoutDashboard},
+    {'label': 'Documents', 'href': '/documents', 'icon' : FolderOpen },
+    {'label': 'Images', 'href': '/images' , 'icon' : Images},
+    {'label': 'Media', 'href': '/media', 'icon' : Video},
+    {'label': 'Others', 'href': '/others', 'icon' : Goal},
 ]
+
+{/* <Video color="#7d0d0d" /> */}

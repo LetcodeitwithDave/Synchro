3ce9dd08b0c38f86155f256de97a407300a27833
Dave <sololadavid4@gmail.com>
2024-11-16 08:00:02 +0100

    refactor component

---
+++ frontend/Synchro/src/components/ChartandDashboardcard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChartandDashboardcard = () => {
+  return <div className=" h-40 w-1/2 rounded-xl bg-secondaryColor " />;
+};
+
+export default ChartandDashboardcard;

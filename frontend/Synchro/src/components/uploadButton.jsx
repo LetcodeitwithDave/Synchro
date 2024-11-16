0c2617fbb2c035a7b10ab3cb48573037799eeacd
Dave <sololadavid4@gmail.com>
2024-11-16 01:01:36 +0100

    change upload icon

--- frontend/Synchro/src/components/uploadButton.jsx
+++ frontend/Synchro/src/components/uploadButton.jsx
@@ -1,33 +1,16 @@
 import { Link } from "react-router-dom";
+import { FileUp, Upload } from "lucide-react";
 
 export const UploadButton = () => {
   return (
     <Link to={"/upload"}>
-      <button className=" flex p-4 items-center text-sm font-rubikRegalar gap-2 bg-secondaryColor text-white rounded-full  h-9">
+      <button className=" flex p-5 items-center  shadow-xl text-sm font-rubikRegalar gap-2 bg-secondaryColor text-white rounded-full  h-9">
         {/* svg */}
         <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="lucide lucide-file-up text-white"
-          >
-            <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
-            <path d="M14 2v4a2 2 0 0 0 2 2h4" />
-            <path d="M12 12v6" />
-            <path d="m15 15-3-3-3 3" />
-          </svg>
+          <Upload className=" text-white text-sm w-5 h-5" />
         </div>
 
-        <div>
-          <h1>UPLOAD A NEW FILE</h1>
-        </div>
+        <h1 className=" font-rubikRegalar text-white text-sm">Upload</h1>
       </button>
     </Link>
   );

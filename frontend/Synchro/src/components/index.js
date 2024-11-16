69a6b06e24f1e855366bdcc5b7172bb4754b03fa
Dave <sololadavid4@gmail.com>
2024-11-16 12:17:17 +0100

    inde

--- frontend/Synchro/src/components/index.js
+++ frontend/Synchro/src/components/index.js
@@ -1,7 +1,11 @@
 import SearchInput from "./SearchInput";
 import Sidebar from "./Sidebar";
+import RecentFileUpload from "./RecentFileUpload";
+import ChartandDashboardcard from "./ChartandDashboardcard";
 
 export{
     SearchInput,
     Sidebar, 
+    RecentFileUpload,
+    ChartandDashboardcard
 }

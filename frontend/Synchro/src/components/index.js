e95b14dbbf09b13cb8c28b29b01a2a5270d8737f
Dave <sololadavid4@gmail.com>
2024-11-18 16:16:08 +0100

    add Navbar components

--- frontend/Synchro/src/components/index.js
+++ frontend/Synchro/src/components/index.js
@@ -2,10 +2,14 @@ import SearchInput from "./SearchInput";
 import Sidebar from "./Sidebar";
 import RecentFileUpload from "./RecentFileUpload";
 import ChartandDashboardcard from "./ChartandDashboardcard";
+import { UploadButton } from "./uploadButton";
+import Navbar from "./Navbar";
 
 export{
     SearchInput,
     Sidebar, 
     RecentFileUpload,
-    ChartandDashboardcard
+    ChartandDashboardcard,
+    UploadButton,
+    Navbar
 }

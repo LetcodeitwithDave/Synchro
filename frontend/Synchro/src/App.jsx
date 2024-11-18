28b1e396f90ef266438e526a5b61b46fecfcc03d
Dave <sololadavid4@gmail.com>
2024-11-18 16:18:47 +0100

    refactor code (component structure)

--- frontend/Synchro/src/App.jsx
+++ frontend/Synchro/src/App.jsx
@@ -8,26 +8,45 @@ import {
   Upload,
   Documents,
 } from "./pages";
+import { Sidebar, SearchInput, UploadButton, Navbar } from "./components";
 
 // import { ToastContainer } from "react-toastify";
 // import "react-toastify/dist/ReactToastify.css";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+// import icon
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
 
 function App() {
   return (
     <Router>
       <AuthProvider>
+        <div className="  flex">
+          <Sidebar />
+
+          <div className="flex flex-col w-full">
+            <Navbar />
+            <Routes>
+              <Route element={<Signup />} path="/register" />
+              <Route element={<Signin />} path="/login" />
+              <Route element={<FileUploadAndList />} path="/" />
+              <Route element={<Dashboard />} path="/home" />
+              <Route element={<Upload />} path="/upload" />
+              <Route element={<Documents />} path="/documents" />
+            </Routes>
+          </div>
+        </div>
         {/* <ToastContainer /> Place it here so it’s available globally */}
-        <Routes>
-          <Route element={<Signup />} path="/register" />
-          <Route element={<Signin />} path="/login" />
-          <Route element={<FileUploadAndList />} path="/" />
-          <Route element={<Dashboard />} path="/home" />
-          <Route element={<Upload />} path="/upload" />
-          <Route element={<Documents />} path="/documents" />
-        </Routes>
       </AuthProvider>
     </Router>
   );
 }
 
 export default App;
+library.add(fab, fas, far);
+
+{
+  /* <FontAwesomeIcon icon="fa-solid fa-image" /> */
+}

0343bb32e45b1da4d91c5a9d9b85bfd4b58e913a
Dave <sololadavid4@gmail.com>
2024-11-19 20:38:32 +0100

    index1111

---
+++ frontend/Synchro/src/constants/index.jsx
@@ -0,0 +1,21 @@
+import { LayoutDashboard, FolderOpen, Video, Goal } from "lucide-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faImage, faFolderOpen } from "@fortawesome/free-solid-svg-icons";
+
+export const Navlinks = [
+  { label: "Dashboard", href: "/home", icon: LayoutDashboard },
+  {
+    label: "Documents",
+    href: "/documents",
+
+    icon: (props) => (
+      <FontAwesomeIcon icon={faFolderOpen} size="xl" {...props} />
+    ),
+  },
+  {
+    label: "Images",
+    href: "/images",
+    icon: (props) => <FontAwesomeIcon icon={faImage} size="xl" {...props} />,
+  },
+  { label: "Media", href: "/media", icon: Video },
+];

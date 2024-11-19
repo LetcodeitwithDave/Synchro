d2d333b0a317f0796f56d09c9e7c64b45530222a
Dave <sololadavid4@gmail.com>
2024-11-19 20:50:20 +0100

    refactor code

--- frontend/Synchro/src/components/Sidebar.jsx
+++ frontend/Synchro/src/components/Sidebar.jsx
@@ -7,52 +7,48 @@ export default function Sidebar() {
   const location = useLocation();
 
   return (
-    <div className=" bg-white  flex flex-col items-start  px-6 min-h-screen   mt-8">
-      <div className=" mx-4 ">
-        <div className=" flex flex-row gap-2 items-center ">
+    <div className="bg-white flex flex-col items-start px-6 min-h-screen mt-8">
+      <div className="mx-4">
+        <div className="flex flex-row gap-2 items-center">
           <div>
-            <CloudUpload className=" p-2 w-full h-full rounded-md bg-secondaryColor text-lg text-white" />
+            <CloudUpload className="p-2 w-full h-full rounded-md bg-secondaryColor text-lg text-white" />
           </div>
-
-          <h1 className=" font-rubikSemiBold text-2xl text-gray-900">
-            Synchro
-          </h1>
+          <h1 className="font-rubikSemiBold text-2xl text-gray-900">Synchro</h1>
         </div>
 
-        <div className=" mt-12">
+        <div className="mt-12">
           {Navlinks.map((items) => (
-            <Link to={items.href}>
+            <Link to={items.href} key={items.label}>
               <button
-                className={`mt-4 font-rubikRegalar py-4 pr-16 p-6   ${
-                  location.pathname == items.href &&
-                  "bg-secondaryColor hover:text-white  transition duration-200 text-white rounded-full shadow-xl"
-                }  text-md text-gray-700 hover:text-secondaryColor gap-4 flex `}
-                key={items.label}
+                className={`mt-4 font-rubikRegalar py-4 pr-16 p-6 ${
+                  location.pathname === items.href &&
+                  "bg-secondaryColor hover:text-white transition duration-200 text-white rounded-full shadow-xl"
+                } text-md text-gray-700 hover:text-secondaryColor gap-4 flex`}
               >
-                {location.pathname == items.href ? (
-                  <items.icon className=" fill-current text-white" />
-                ) : (
-                  <items.icon className=" fill-current text-gray-400" />
-                )}
-
+                <items.icon
+                  className={`${
+                    location.pathname === items.href
+                      ? "text-white"
+                      : "text-gray-400"
+                  } fill-current`}
+                />
                 {items.label}
               </button>
             </Link>
           ))}
         </div>
 
-        <div className=" flex flex-col items-center  mt-14 ">
+        <div className="flex flex-col items-center mt-14">
           <img
             src={illustrationDashboard}
             alt="dashboard illustration"
-            className=" w-56"
+            className="w-56"
           />
-          <div className=" flex gap-4 items-center  mt-10 ">
-            <div className=" h-14  w-14  rounded-full bg-gray-300" />
-
+          <div className="flex gap-4 items-center mt-10">
+            <div className="h-14 w-14 rounded-full bg-gray-300" />
             <div>
-              <h1 className=" font-rubikSemiBold">Solola David</h1>
-              <p className=" font-rubikRegalar text-gray-500">
+              <h1 className="font-rubikSemiBold">Solola David</h1>
+              <p className="font-rubikRegalar text-gray-500">
                 sololadavid4@gmail.com
               </p>
             </div>

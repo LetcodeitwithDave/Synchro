7ad14dad59f18dff1cf6d83b05382a1dfa3a235c
Dave <sololadavid4@gmail.com>
2024-11-21 21:31:28 +0100

    register File model

--- backend/documents/admin.py
+++ backend/documents/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import File, Tag, Category
+from .models import Document, Tag, Category, File
 # Register your models here.
 
 
-admin.site.register(File)
+admin.site.register(Document)
 admin.site.register(Tag)
 admin.site.register(Category)
+admin.site.register(File)

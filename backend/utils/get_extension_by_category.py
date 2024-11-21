bf71689d6366e9ceac5bfab752f2db937ea47857
Dave <sololadavid4@gmail.com>
2024-11-21 20:28:21 +0100

    custom fucntion to determine category based on file extension

---
+++ backend/utils/get_extension_by_category.py
@@ -0,0 +1,20 @@
+import os
+
+FILE_CATEGORIES = {
+    "documents": [".pdf", ".doc", ".docx", ".txt", ".csv", ".fig", ".sketch", ".xd"],
+    "images": [".jpg", ".jpeg", ".png", ".gif", ".svg"],
+    "media": [".mp4", ".mp3", ".wav", ".mkv"],
+    "others": []  # Files that don't fit other categories
+}
+
+def get_category_by_extension(file_extension):
+    """Determine the category of a file based on its extension."""
+    for category, extensions in FILE_CATEGORIES.items():
+        if file_extension.lower() in extensions:
+            print(category)
+            print(file_extension.lower())
+            print(extensions)
+            print(FILE_CATEGORIES.items())
+    return print("others") 
+
+get_category_by_extension('.sVg')

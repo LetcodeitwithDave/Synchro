da7875b845aa3cb4695c4f19d6ed136b0f882f74
Dave <sololadavid4@gmail.com>
2024-11-27 16:47:22 +0100

    register CatgoryClass model to admin site

--- backend/documents/admin.py
+++ backend/documents/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Document, Tag, Category, File
+from .models import Document, Tag, Category, File, CategoryClass
 # Register your models here.
 
 
@@ -7,3 +7,4 @@ admin.site.register(Document)
 admin.site.register(Tag)
 admin.site.register(Category)
 admin.site.register(File)
+admin.site.register(CategoryClass)

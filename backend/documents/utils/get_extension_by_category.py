bb1b90fd28fe7edce833a397ed702df16238a10b
Dave <sololadavid4@gmail.com>
2024-11-27 18:22:30 +0100

    fix fILE  category issues

--- backend/documents/utils/get_extension_by_category.py
+++ backend/documents/utils/get_extension_by_category.py
@@ -1,22 +1,29 @@
 import os
-
+from django.apps import apps
+from documents.models import CategoryClass
 FILE_CATEGORIES = {
-    "documents": [".pdf", ".doc", ".docx", ".txt", ".csv", ".fig", ".sketch", ".xd"],
-    "images": [".jpg", ".jpeg", ".png", ".gif", ".svg"],
+    "documents": [".docx", ".pdf", ".doc", ".txt", ".csv", ".fig", ".sketch", ".xd"],
+    "images": [".jpg", ".jpeg", ".gif", ".svg", ".png"],
     "media": [".mp4", ".mp3", ".wav", ".mkv"],
     "others": []  # Files that don't fit other categories
 }
 
 def get_category_by_extension(file_extension):
-    """Determine the category of a file based on its extension."""
-    for category, extensions in FILE_CATEGORIES.items():
+    print('file in funciotn => ', file_extension)
+    """Detrmine the category of a file based on its extension."""
+    for category_name, extensions in FILE_CATEGORIES.items():
+        print('extension => ', extensions)
         if file_extension.lower() in extensions:
-            print(category)
+
+            print(category_name)
             print(file_extension.lower())
             print(extensions)
+
+            category,_ = CategoryClass.objects.get_or_create(name=category_name)
    
 
             return category
+    category,_ = CategoryClass.objects.get_or_create(name=category_name)
     return category
 
-# get_category_by_extension('.test')
+# get_category_by_extension('.mp3')

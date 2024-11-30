861c739f59a145e7c7136057b4f1cdee3a9ab59f
Dave <sololadavid4@gmail.com>
2024-11-30 16:31:47 +0100

    .

--- frontend/Synchro/src/components/RecentFileUpload.jsx
+++ frontend/Synchro/src/components/RecentFileUpload.jsx
@@ -6,8 +6,8 @@ const RecentFileUpload = ({ recentUpload }) => {
       <h1 className=" font-rubikSemiBold text-gray-900 text-2xl ">
         Recent files uploaded
       </h1>
-      {recentUpload.map((item) => (
-        <div className=" flex gap-2 items-center  mt-10 ">
+      {recentUpload.map((item, index) => (
+        <div className=" flex gap-2 items-center  mt-10 " key={index}>
           <div className=" h-14  w-14  rounded-full bg-gray-300" />
 
           <div className=" flex items-center gap-40  justify-between ">

dffe0b76c7b03079f1db2716830a36c2cdb07955
Dave <sololadavid4@gmail.com>
2024-12-01 19:47:32 +0100

    recent upload logic

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -2,32 +2,36 @@ from django.shortcuts import render
 from .serializers import DashboardSummarySerializer
 from rest_framework.response import Response
 from rest_framework import status
-from  documents.models import Document
+
 from rest_framework.decorators import api_view
 import os
+
+from documents.models import File
+from documents.serializers import FileSerializer
 
 @api_view(['GET', 'POST'])
 def dashboard_summary (request):
     if request.method == 'GET':
-        recent_documents =  Document.objects.order_by('-upload_date')[:5]
-        serializer = DashboardSummarySerializer(recent_documents, many=True, context={'request': request})
+        recent_documents =  File.objects.order_by('-uploaded_at')[:5]
+        serializer = FileSerializer(recent_documents, many=True, context={'request': request})
 
 
         return Response(serializer.data, status= status.HTTP_200_OK)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
     
-    elif request.method == 'POST':
-        print('request data -> ', request.FILES)
-        print('request data name -> ', request.FILES['file'])
-        print('request data path -> ', os.path.splitext(str(request.FILES['file'])))
-        print('request data path name -> ', os.path.splitext(request.FILES['file'].name))
-        print('request data path name -> ', request.FILES['file'].size)
+    # elif request.method == 'POST':
+    #     print('request data -> ', request.FILES)
+    #     print('request data name -> ', request.FILES['file'])
+    #     print('request data path -> ', os.path.splitext(str(request.FILES['file'])))
+    #     print('request data path name -> ', os.path.splitext(request.FILES['file'].name))
+    #     print('request data path name -> ', request.FILES['file'].size)
        
-        serializer = DashboardSummarySerializer(data= request.data)
+    #     serializer = DashboardSummarySerializer(data= request.data)
 
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     

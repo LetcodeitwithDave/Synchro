5ffc5edc051a6981cc0990d974ea82d98864d16d
Dave <sololadavid4@gmail.com>
2024-12-04 15:35:25 +0100

    adjust endpoint for files based on category

--- backend/documents/urls.py
+++ backend/documents/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    # path("documents", views.mydocuments , name="documents list"),
+
     path("download_document/<int:file_id>", views.download_document, name="download file"),
     path("documents/search", views.search_document, name='search document' ),
     path("documents/upload", views.upload_file, name='uplaod file' ),
-    path("documents", views.document_type, name="document type")
+    path("documents/<str:category_type>", views.document_type, name="document type")
 ]

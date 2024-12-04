da5378743a393db69c598a362b91a529fe39e93a
Dave <sololadavid4@gmail.com>
2024-12-04 15:55:32 +0100

    connect to endpoint

--- frontend/Synchro/src/pages/Images.jsx
+++ frontend/Synchro/src/pages/Images.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from "react";
 // import NavContent from "../components/NavContent";
-export const Images = () => {
-  const [files, setFiles] = useState([]);
+
+const Images = () => {
+  const [images, setImages] = useState([]);
   const FetchFiles = async () => {
     try {
       const response = await fetch(
-        "http://localhost:8000/api/dashboard/dashboard_summary/",
+        "http://localhost:8000/api/documents/image?category=images",
         { method: "GET" }
       );
 
       if (response.ok) {
         const data = await response.json();
-        setFiles(data);
+        setImages(data);
 
         // console.log("files -> ", files);
         // console.log("response data ", data);
@@ -27,10 +28,10 @@ export const Images = () => {
 
   return (
     <div className=" flex">
-      <div className=" flex-col  flex-1 flex ">
+      <div className=" flex-col flex-1 flex ">
         <div className=" flex flex-col ">
           <div className=" font-rubikSemiBold text-gray-900 text-4xl">
-            Documents
+            Images
           </div>
           <div className=" font-rubikRegalar text-md text-gray-700 mt-4 ">
             Total: 12.5Gb
@@ -38,16 +39,11 @@ export const Images = () => {
         </div>
 
         <div className=" grid grid-cols-4  mt-16 gap-10 w-full">
-          <div className=" bg-white h-44 w-60 rounded-xl shadow-md  "></div>
-          <div className=" bg-white h-44 w-60 rounded-xl  shadow-md  "></div>
-          <div className=" bg-white h-44 w-60 rounded-xl shadow-md  "></div>
-          <div className=" bg-white h-44 w-60 rounded-xl shadow-md  "></div>
-          <div className=" bg-white h-44 w-60 rounded-xl shadow-md  "></div>
-          <div className=" bg-white h-44 w-60 rounded-xl  shadow-md "></div>
-          <div className=" bg-white h-44 w-60 rounded-xl  shadow-md "></div>
-          <div className=" bg-white h-44 w-60 rounded-xl  shadow-md "></div>
-          <div className=" bg-white h-44 w-60 rounded-xl  shadow-md "></div>
-          <div className=" bg-white h-44 w-60 rounded-xl  shadow-md "></div>
+          {images.map((image, index) => (
+            <div className=" bg-white h-44 w-60 rounded-xl shadow-md  ">
+              {image.name}{" "}
+            </div>
+          ))}
         </div>
 
         {/* <div className=" mt-8 flex">
@@ -57,3 +53,5 @@ export const Images = () => {
     </div>
   );
 };
+
+export default Images;
